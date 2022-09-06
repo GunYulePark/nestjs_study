c7dc9ef0fb6024fe63302a03bb319239a5a7746b
GunYulePark <85776027+GunYulePark@users.noreply.github.com>
2022-09-06 23:45:53 +0900

    Update 1.ts

--- express_ver_storage/1.ts
+++ express_ver_storage/1.ts
@@ -10,7 +10,7 @@ app.get("/test", (req: express.Request, res: express.Response) => {
   // http://localhost:8000(/) 맨 마지막 주소(/test)로 get을 했을 때 어떻게 응답을 해줄 거니?
   console.log(req);
   res.send({ name: "geon youl park", age: 99, friends: ["ss", "aa", "bb"] });
-  // 클라이언트, 프론트 > 백엔드 req
+  // 클라이언트, 프론트 > 백엔드 request
   // 백엔드는 req를 받고, res.send로 프론트로 응답을 보내준다.(response)
   // 프론트는 그 res를 받아서 ui를 만들어 화면에 송출한다.
 });
